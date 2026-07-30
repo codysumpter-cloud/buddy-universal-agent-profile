e869ebb5a45a0b0b17be49c10c31b4aebb8ebb81
Prismtek <cody.sumpter@gmail.com>
2026-07-30 08:12:56 -0400

    feat(compiler): export agent life compiler

--- packages/buap-compiler/src/index.mjs
+++ packages/buap-compiler/src/index.mjs
@@ -1,4 +1,5 @@
 export { checkProject, compileProject, loadProject, validateProject, writeProject } from "./compiler.mjs";
+export { compileLifeProfile, DEFAULT_AGENT_LIFE } from "./life-profile.mjs";
 export { resolveModuleGraph } from "./graph.mjs";
 export { doctorProject, formatDoctor, initProject } from "./init.mjs";
 export { parseSimpleYaml, readStructuredFile } from "./parser.mjs";
